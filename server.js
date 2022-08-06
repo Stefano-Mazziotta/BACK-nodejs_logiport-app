a72be1add7a8b02b34b42cf6e1f2616913182e08
stefano-mazziotta <mazziottastefano@gmail.com>
2022-08-06 16:38:48 +0200

    Middleware - errorHandler

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const errorHandler = require('./app/middlewares/errorHandler');
 
 var corsOptions = {
     origin: "http://localhost:4200"
@@ -9,17 +10,17 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // parse request of content-type - application/json
-app.use(express.json());
 // parse request of content-type - application/x-www-form-urlencoded
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
 app.get("/", (req, res) => {
     res.json({ message: "welcome to logiport app aplication jeje" });
 });
 
 require("./app/routes/company.routes.js")(app);
 require("./app/routes/boat.routes.js")(app);
+app.use(errorHandler);
 
 //set port, listen for request
 const PORT = process.env.PORT || 8080;
