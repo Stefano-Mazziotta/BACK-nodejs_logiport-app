bfba1740b91b449c46d30ffcaf1eff7a3aba7826
stefano-mazziotta <mazziottastefano@gmail.com>
2022-08-20 13:38:56 +0200

    add "invalid token" validation

--- app/middlewares/errorHandler.js
+++ app/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ function errorHandler(error, req, res, next){
     let errorStatus = error.status || 500;
     let errorMessage = error.message;
 
-    if (errorMessage.startsWith("jwt")){
+    if (errorMessage.startsWith("jwt") || errorMessage === "invalid token"){
         errorStatus = 401;
     }
     
