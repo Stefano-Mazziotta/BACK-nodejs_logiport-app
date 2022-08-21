2719a03580e720913e7384af0c109e029a834684
stefano-mazziotta <mazziottastefano@gmail.com>
2022-08-21 02:00:35 +0200

    [fix] cleanLineBreak - replace multiple spaces with a single space

--- app/utils/stringUtils.js
+++ app/utils/stringUtils.js
@@ -1,7 +1,10 @@
 const stringUtils = {
     
-    cleanLineBreak: (str) => {
-        return str.replace(/(\r\n|\n|\r)/gm, "");
+    cleanLineBreak: (string) => {
+        string = string.replace(/(\r\n|\n|\r)/gm, "");
+        string = string.replace(/\s\s+/g, ' ');
+
+        return string;
     }
 }
 
